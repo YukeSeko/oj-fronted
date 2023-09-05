fa071d64b346d841a98ae337fcff6cacc28d159d
wzy <1286694258@qq.com>
2023-09-06 00:03:09 +0800

    first commit

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,76 @@
+<template>
+  <a-row id="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.png" />
+            <div class="title">OJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in routes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        {{ store.state.user?.loginUser?.userName ?? "未登录" }}
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { routes } from "@/router/routers";
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const router = useRouter();
+// 默认主页
+const selectedKeys = ref(["/"]);
+
+// 路由跳转后，更新选中的菜单项
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+// setTimeout(() => {
+//   store.dispatch("user/getLoginUser", {
+//     userName: "鱼皮管理员",
+//     userRole: ACCESS_ENUM.ADMIN,
+//   });
+// }, 3000);
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
