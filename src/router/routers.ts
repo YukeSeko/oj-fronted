5d03f743284f2775777b1c272efa8310ab493b6f
王灼宇 <1286694258@qq.com>
2023-09-07 17:28:00 +0800

    添加依赖、添加全局权限校验

--- src/router/routers.ts
+++ src/router/routers.ts
@@ -1,19 +1,85 @@
 import { RouteRecordRaw } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import NoAuthView from "@/views/NoAuthView.vue";
+import ACCESS_ENUM from "@/access/accessEnum";
 
 export const routes: Array<RouteRecordRaw> = [
+  // {
+  //   path: "/user",
+  //   name: "用户",
+  //   component: UserLayout,
+  //   children: [
+  //     {
+  //       path: "/user/login",
+  //       name: "用户登录",
+  //       component: UserLoginView,
+  //     },
+  //     {
+  //       path: "/user/register",
+  //       name: "用户注册",
+  //       component: UserRegisterView,
+  //     },
+  //   ],
+  //   meta: {
+  //     hideInMenu: true,
+  //   },
+  // },
+  // {
+  //   path: "/questions",
+  //   name: "浏览题目",
+  //   component: QuestionsView,
+  // },
+  // {
+  //   path: "/question_submit",
+  //   name: "浏览题目提交",
+  //   component: QuestionSubmitView,
+  // },
+  // {
+  //   path: "/view/question/:id",
+  //   name: "在线做题",
+  //   component: ViewQuestionView,
+  //   props: true,
+  //   meta: {
+  //     access: ACCESS_ENUM.USER,
+  //     hideInMenu: true,
+  //   },
+  // },
+  // {
+  //   path: "/add/question",
+  //   name: "创建题目",
+  //   component: AddQuestionView,
+  //   meta: {
+  //     access: ACCESS_ENUM.USER,
+  //   },
+  // },
+  // {
+  //   path: "/update/question",
+  //   name: "更新题目",
+  //   component: AddQuestionView,
+  //   meta: {
+  //     access: ACCESS_ENUM.USER,
+  //     hideInMenu: true,
+  //   },
+  // },
+  // {
+  //   path: "/manage/question/",
+  //   name: "管理题目",
+  //   component: ManageQuestionView,
+  //   meta: {
+  //     access: ACCESS_ENUM.ADMIN,
+  //   },
+  // },
+  // {
+  //   path: "/",
+  //   name: "主页",
+  //   component: QuestionsView,
+  // },
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/noAuth",
+    name: "无权限",
+    component: NoAuthView,
+    meta: {
+      hideInMenu: true,
+    },
   },
 ];
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,7 +1,6 @@
-// initial state
 import { StoreOptions } from "vuex";
-// import ACCESS_ENUM from "@/access/accessEnum";
-// import { UserControllerService } from "../../generated";
+import { UserControllerService } from "@/api";
+import ACCESS_ENUM from "@/access/accessEnum";
 
 export default {
   namespaced: true,
@@ -11,18 +10,18 @@ export default {
     },
   }),
   actions: {
-    // async getLoginUser({ commit, state }, payload) {
-    //   // 从远程请求获取登录信息
-    //   const res = await UserControllerService.getLoginUserUsingGet();
-    //   if (res.code === 0) {
-    //     commit("updateUser", res.data);
-    //   } else {
-    //     commit("updateUser", {
-    //       ...state.loginUser,
-    //       userRole: ACCESS_ENUM.NOT_LOGIN,
-    //     });
-    //   }
-    // },
+    async getLoginUser({ commit, state }, payload) {
+      // 从远程请求获取登录信息
+      const res = await UserControllerService.getLoginUserUsingGet();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: ACCESS_ENUM.NOT_LOGIN,
+        });
+      }
+    },
   },
   mutations: {
     updateUser(state, payload) {
